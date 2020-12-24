2562d899491d90fe5d695ed64db5e8b9b506979b
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-24 18:55:39 -0400

    passed sce-1-1-2

--- Convert/test/Convert_test.dart
+++ Convert/test/Convert_test.dart
@@ -5,4 +5,8 @@ void main() {
   test('Convert decimal 1 to hexadecimal', () {
     expect(convertToHex(1), '1');
   });
+
+  test('Convert decimal 232  to hexadecimal', () {
+    expect(convertToHex(232), 'E8');
+  });
 }
