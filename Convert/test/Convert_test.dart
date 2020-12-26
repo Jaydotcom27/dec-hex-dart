e95fd9358fb437345d76ffd031dc35703c374328
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-25 21:22:50 -0400

    passed sce-1-1-1

--- Convert/test/Convert_test.dart
+++ Convert/test/Convert_test.dart
@@ -1,4 +1,8 @@
 import 'package:Convert/Convert.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  test('SCE-1-1-1: Convert decimal 1 to hexadecimal', () {
+    expect(convertToHex(1), '1');
+  });
+}
