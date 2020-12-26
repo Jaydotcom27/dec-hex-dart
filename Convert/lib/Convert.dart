055884ab4ed0b84c245785db80c0f8fdb17a5a6d
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-25 21:26:00 -0400

    passed sce-1-1-2

--- Convert/lib/Convert.dart
+++ Convert/lib/Convert.dart
@@ -1,3 +1,25 @@
 String convertToHex(n) {
-  return n.toString();
+  var bs = '';
+  var r = 0;
+
+  while (n != 0) {
+    r = n % 16;
+    if (r == 10) {
+      bs += 'A';
+    } else if (r == 11) {
+      bs += 'B';
+    } else if (r == 12) {
+      bs += 'C';
+    } else if (r == 13) {
+      bs += 'D';
+    } else if (r == 14) {
+      bs += 'E';
+    } else if (r == 15) {
+      bs += 'F';
+    } else {
+      bs += r.toString();
+    }
+    n = (n ~/ 16);
+  }
+  return bs.split('').reversed.join('');
 }
--- Convert/test/Convert_test.dart
+++ Convert/test/Convert_test.dart
@@ -5,4 +5,8 @@ void main() {
   test('SCE-1-1-1: Convert decimal 1 to hexadecimal', () {
     expect(convertToHex(1), '1');
   });
+
+  test('SCE-1-1-2: Convert decimal 213123 to hexadecimal', () {
+    expect(convertToHex(213123), '34083');
+  });
 }
