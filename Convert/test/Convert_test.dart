e33aa52f552425178493dde61cc9abf005bda1b8
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-12-25 21:14:34 -0400

    deleted day 2 kata

--- Convert/test/Convert_test.dart
+++ Convert/test/Convert_test.dart
@@ -1,24 +1,4 @@
 import 'package:Convert/Convert.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('Convert decimal 1 to hexadecimal', () {
-    expect(convertToHex(1), '1');
-  });
-
-  test('Convert decimal 232 to hexadecimal', () {
-    expect(convertToHex(232), 'E8');
-  });
-
-  test('Convert decimal 23221 to hexadecimal', () {
-    expect(convertToHex(23221), '5AB5');
-  });
-
-  test('Convert decimal 699 to hexadecimal', () {
-    expect(convertToHex(699), '2BB');
-  });
-
-  test('Convert decimal 3847 to hexadecimal', () {
-    expect(convertToHex(3847), 'F07');
-  });
-}
+void main() {}
